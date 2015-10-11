aade3cea07468441fc7c2de77db42cb9b08942ac
Koji Shimba <me@shimba.co>
2015-10-11 21:30:50 +0900

    h1~h6のid属性にユニークな文字列を追加

--- lib/highlighter.rb
+++ lib/highlighter.rb
@@ -5,5 +5,19 @@ require "rouge/plugins/redcarpet"
 module Highlighter
   class HighlightedHTML < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
+
+    def header(text, level)
+      "<h#{level} class='anchorable-toc js-anchorable-toc' id='#{anchorify(text)}'>#{text}</h#{level}>"
+    end
+
+    private
+
+    def anchorify(text)
+      slug = text.gsub(/&#?\w+;/, '-').gsub(/\W+/, '-').gsub(/^-|-$/, '').downcase
+      suffix = Digest::SHA256.hexdigest(text)[0, 7]
+
+      return suffix if slug.blank?
+      "#{slug}-#{suffix}"
+    end
   end
 end
